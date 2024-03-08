7c59e4ddad12f765587d1b834f9dbce991d2f37f
s27236 <s27236@pjwstk.edu.pl>
2024-03-08 16:51:09 +0100

    Modification 3

--- APBD1/APBD1/Program.cs
+++ APBD1/APBD1/Program.cs
@@ -1,14 +1,4 @@
 ﻿// See https://aka.ms/new-console-template for more information
 
 
-int a = 5;
-
-int b = 100;
-
-while (a > 0)
-{
-    Console.WriteLine(a);
-    Console.WriteLine(b);
-    a -= 1;
-    b /= 2;
-}
+Console.WriteLine("SkibidiBeridze");
